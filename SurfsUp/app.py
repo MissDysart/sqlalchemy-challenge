941dd362974f57f1afed996d33af1e6ade12dcb1
MissDysart <kelsy.dysart@gmail.com>
2024-02-25 16:22:56 -0700

    Nearly finish climate data and start app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,69 @@
 # Import the dependencies.
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
-
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def homepage():
+    """All available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Get query results of precipitation analysis
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    year_prcp = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    # session.close()
+    # Convert to dictionary
+    past_year_prcp = []
+    for date,prcp in year_prcp:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        past_year_prcp.append(prcp_dict)
+
+    return jsonify(past_year_prcp)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
